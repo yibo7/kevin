957a31a7251f3aa2c8ff52963cb58ed9a3194f4e
yibo7 <58937236+yibo7@users.noreply.github.com>
2021-12-25 16:24:28 +0800

    for

--- test.py
+++ test.py
@@ -1,10 +1,2 @@
-# class_room = [1,1,2,3,4,5,6]
-import time
-# for i in range(1,10):
-#     print(i)
-num = 1
-while num<10:
-    
-    time.sleep(1)
-    num = num+1
-    print(f"fffff{num}") 
+for i in range(1,10): 
+    print(f'执行了{i}')
